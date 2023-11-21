3606fb5c7b5c9010e1365ce2202456fd2c968f0f
Guillaume Thomas <guillaume.thomas2@gmail.com>
2023-11-21 11:57:56 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -142,6 +142,7 @@ github_download "edenhill/yajl" "edenhill" "libyajl"
 build libyajl "([ -d build ] || ./configure --prefix $DEST) && make install" || (echo "Failed to build libyajl: JSON support will probably be disabled" ; true)
 
 download http://www.digip.org/jansson/releases/jansson-2.12.tar.gz libjansson
+download http://www.digip.org/jansson/releases/jansson-2.12.tar.gz libjansson
 build libjansson "([[ -f config.status ]] || ./configure --enable-static --prefix=$DEST) && make && make install" || (echo "Failed to build libjansson: AVRO support will probably be disabled" ; true)
 
 github_download "apache/avro" "release-1.8.2" "avroc"
